6264306ebb1b45f1007d49522fa320265820cf9e
Mészáros Gergely <grigorijat9026@gmail.com>
2020-12-03 23:08:23 +0100

    removed deprecated approaches of getting competitor results

--- comp/competitorService.go
+++ comp/competitorService.go
@@ -4,7 +4,6 @@ import (
 	"github.com/gregito/vrviewer/comp/common"
 	"github.com/gregito/vrviewer/comp/dto"
 	"github.com/gregito/vrviewer/comp/model"
-	"log"
 	"strconv"
 	"sync"
 )
@@ -26,26 +25,6 @@ func GetCompetitorResults(name string, cds []model.CompetitionDetail) []dto.Comp
 	return result
 }
 
-func GetCompetitorParticipationResults(competitorName string) []dto.CompetitorResult {
-	var result []dto.CompetitorResult
-	comps := ListAllCompetitionsSimplified()
-	wg := sync.WaitGroup{}
-	for _, competition := range comps {
-		wg.Add(1)
-		go func(competition dto.Competition) {
-			comp, err := GetCompetitorOnCompetitionByCompetitionIdAndCompetitorName(competition.ID, competitorName)
-			if err != nil {
-				log.Println(err)
-			} else {
-				result = append(result, comp)
-			}
-			wg.Done()
-		}(competition)
-	}
-	wg.Wait()
-	return result
-}
-
 func GetCompetitorResultInCompetitionDetail(name string, cd model.CompetitionDetail) dto.CompetitorResult {
 	result := findResultOfCompetitor(name, cd)
 	sectionResult := findSectionResultOfCompetitor(name, result)
@@ -59,26 +38,6 @@ func GetCompetitorResultInCompetitionDetail(name string, cd model.CompetitionDet
 		CurrentPosition: strconv.FormatInt(result.Position, 10),
 		SectionResults:  sections,
 	}
-}
-
-func GetCompetitorOnCompetitionByCompetitionIdAndCompetitorName(competitionId int64, competitorName string) (dto.CompetitorResult, error) {
-	comp, err := GetCompetitionResultsByCompetitionId(competitionId)
-	if err != nil {
-		log.Println(err)
-		return dto.CompetitorResult{}, err
-	}
-	result := findResultOfCompetitor(competitorName, comp)
-	sectionResult := findSectionResultOfCompetitor(competitorName, result)
-	var sections []dto.Section
-	for _, s := range sectionResult {
-		sections = append(sections, convertSectionResultAndSectionMapToSectionDto(s, comp.Sections))
-	}
-	return dto.CompetitorResult{
-		CompetitionName: comp.Name,
-		Name:            competitorName,
-		CurrentPosition: strconv.FormatInt(result.Position, 10),
-		SectionResults:  sections,
-	}, nil
 }
 
 func findResultOfCompetitor(name string, cd model.CompetitionDetail) model.Result {
